93a40f8e0922cb665539eeb96ca087a027d125e8
Kamsiyochukwu <okonkwokamsi1@gmail.com>
2022-06-04 13:14:18 +0000

    Temporary changes

--- final_project/server.py
+++ final_project/server.py
@@ -8,17 +8,24 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
+    english_text = input('Enter text in English - ')
     return "Translated text to French"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
+    french_text = input('Enter text in French - ')
     return "Translated text to English"
 
-@app.route("/")
+print(englishToFrench())
+print(frenchToEnglish())
+
+@app.route("/index.html")
 def renderIndexPage():
     # Write the code to render template
+    return "Translated text to French"
+    return "Translated text to English"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
